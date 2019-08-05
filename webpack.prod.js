5892b7b4af8023740bba239faf1cfda60c5fdfad
韩子卢 <vvjiang@foxmail.com>
2019-08-05 11:14:32 +0800

    去除打包分析并消除github安全性警告

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
@@ -18,7 +17,6 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    new BundleAnalyzerPlugin({ analyzerPort: 8919 }),
     // new webpack.optimize.CommonsChunkPlugin({
     //   names: ['vendor'],
     //   minChunks: Infinity,
